6d632f6c2d684f93a768ca95feee374aca63b077
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-04-19 18:11:13 +0800

    add cloudfront

--- main.tf
+++ main.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "image_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "allow_public_policy" {
+  bucket = aws_s3_bucket.image_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_sqs_queue" "image_queue" {
   name                      = "image-compression-queue"
   visibility_timeout_seconds = 300
@@ -44,6 +53,23 @@ resource "aws_sqs_queue_policy" "s3_to_sqs_policy" {
             "aws:SourceArn" = aws_s3_bucket.image_bucket.arn
           }
         }
+      }
+    ]
+  })
+}
+
+# S3 Bucket Policy to allow public read for optimized folder
+resource "aws_s3_bucket_policy" "optimized_folder_policy" {
+  bucket = aws_s3_bucket.image_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.image_bucket.arn}/optimized/*"
       }
     ]
   })
@@ -133,5 +159,51 @@ resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
   function_name    = aws_lambda_function.compress_image.arn
   batch_size       = 1
   enabled          = true
+}
+
+# CloudFront Distribution for serving optimized images
+resource "aws_cloudfront_distribution" "image_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.image_bucket.bucket_regional_domain_name
+    origin_id   = "s3-origin-optimized"
+
+    origin_path = "/optimized"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for optimized images"
+  default_root_object = ""
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin-optimized"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  price_class = "PriceClass_100" # Cheapest option (US, Canada, Europe)
 }
 
